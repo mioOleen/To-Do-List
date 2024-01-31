15e48637b5f9c1acaffedb0f8534cc73675b49d4
zintheint <zintheint228@gmail.com>
2024-01-31 16:39:45 +0630

    added clear all task icon and made the tasks draggable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import _ from 'lodash';
+import _, { iteratee } from 'lodash';
 import './style.css';
 import '../node_modules/boxicons/css/boxicons.min.css';
 
 const userInput = document.getElementById("userInput");
 const btn = document.getElementById("btn");
 const lists = document.getElementById('list');
+const clearButton = document.getElementById('binForAll');
+
+let draggedItem=null;
 
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const saveData=()=>{
     localStorage.setItem('tasks',JSON.stringify(tasks));
 }
+
+clearButton.addEventListener('click',function(){
+    localStorage.clear();
+    location.reload();
+})
 
 const addTask= () =>{
     if(userInput.value != ''){
@@ -39,6 +47,8 @@ const renderTasks=()=>{
     tasks.forEach((item,index) =>{ 
         let li = document.createElement('li');
         li.classList.add('item');
+        li.setAttribute('draggable',true);
+        li.dataset.index = index;
 
         let div1 = document.createElement('div');
         let circle = document.createElement('i');
@@ -61,29 +71,62 @@ const renderTasks=()=>{
         let span = document.createElement('span');
         span.setAttribute('contenteditable',true);
         span.innerText = item.task;
-
+       
         span.addEventListener('blur', function () {
             tasks[index].task = span.innerText;
             saveData();
         });
 
-        div1.append(circle);
-        div1.append(span);
-        
         let div2 = document.createElement('div');
         let del = document.createElement('i');
         del.classList.add('bx','bxs-trash-alt');
         del.addEventListener('click',function(){
             deleteTask(index);
         })
+        
+        let dragIcon = document.createElement('i');
+        dragIcon.classList.add('bx','bx-dots-vertical-rounded');
+        
+        dragIcon.style.cursor = 'move';
+        li.addEventListener('dragstart',function(e){
+            draggedItem =li;
+            e.dataTransfer.effectAllowed = 'move';
+        });
+        
+        li.addEventListener('dragover',function(e){
+            e.preventDefault();
+            this.classList.add('dragged-over');
+        });
+        
+        li.addEventListener('dragleave',function(){
+            this.classList.remove('dragged-over');
+        })
+        
+        li.addEventListener('drop', function () {
+            if (draggedItem !== null) {
+                const fromIndex = tasks.findIndex((item) => item.task === draggedItem.querySelector('span').innerText);
+                const toIndex = tasks.findIndex((item) => item.task === li.querySelector('span').innerText);
+                
+                [tasks[fromIndex], tasks[toIndex]] = [tasks[toIndex], tasks[fromIndex]];
+                
+                tasks.forEach((item, i) => {
+                    item.index = i + 1;
+                });       
+                saveData();
+                renderTasks();      
+                draggedItem = null;
+            }
+        });
+           
+        div1.append(circle);
+        div1.append(span);
         div2.append(del);
+        div2.append(dragIcon);
         li.append(div1);
         li.append(div2);
         lists.append(li);
-
     })
 }
-
 userInput.addEventListener("keydown", function(event) {
     if(userInput.value != ''){
         if (event.keyCode === 13) {
